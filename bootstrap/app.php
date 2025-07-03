5be48cdf4709930774d4694f50859d03ee83b30b
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2025-07-03 09:15:21 +0100

    fix: testing bugs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,8 +23,9 @@ return Application::configure(basePath: dirname(__DIR__))
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // $middleware->api(prepend: [
-        // ]);
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
